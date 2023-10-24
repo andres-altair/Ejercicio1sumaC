270ab8a96edcdd44aac5e77289ec2f7f41ac73e8
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-10-24 14:21:23 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ejercicio1sumaC;
+using Ejercicio1sumaC.Servicios;
+
+namespace Ejercicio1sumaC
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            pedirI pI = new pedirC();
+            int i = pI.pedir();
+            SumaI sI = new SumaC();
+            i = sI.suma(int n1);
+
+        }
+    }
+}
+
+
---
+++ Servicios/SumaC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1sumaC.Servicios
+{
+    internal class SumaC : SumaI
+    {
+        public void suma(int n1)
+        {
+            int n2;
+            for (int i = 0; i <= n1; i++)  
+            {
+                n2 += i;
+              
+            }
+            Console.WriteLine(n2"="i+);
+
+        }
+    }
+}
